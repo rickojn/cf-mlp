06d2d3885115d658a49572bed1cd06fe7e30ff82
rickojn <rickojn@hotmail.com>
2026-01-21 23:16:25 +0000

    debug

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-export DATA_PATH=/home/patrick/coding/mnist-data/
-export MODELS_PATH=/home/patrick/coding/cf-mlp/models/
-export Torch_DIR=/home/patrick/libtorch/libtorch
+export DATA_PATH=/home/xxxxxx/coding/mnist-data/
+export MODELS_PATH=/home/xxxxxx/coding/cf-mlp/models/
+export Torch_DIR=/home/xxxxxx/libtorch/libtorch
+
+# to build debug version:
+# cmake -DCMAKE_BUILD_TYPE=Debug -B build-debug
+# cmake --build build-debug
